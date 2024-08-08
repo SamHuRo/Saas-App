38e61802a012d992da5108031e91ffe8ab679d48
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-08-08 11:48:57 -0500

    Actualization of home page

--- src/home/views.py
+++ src/home/views.py
@@ -1,6 +1,16 @@
+import pathlib
+from django.shortcuts import render
 from django.http import HttpResponse
 
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
 
-    return HttpResponse("<h1>Hola mundo</h1>")
+    #Html document direction
+    html_tamplate = "home.html"
+
+    return render(request, html_tamplate, my_context)
